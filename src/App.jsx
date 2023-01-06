4c5cee7c9287872f1a59c743bc9a1ead757dce0a
Gabriel Telles <gbertelles@gmail.com>
2023-01-06 16:12:52 -0300

    Adding the final features

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,7 @@ export function App() {
           {posts.map(post => {
             return (
               <Post 
+                key={post.id}
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
